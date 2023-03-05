66c7706d5596c5c25ac7bad4ee93582a1456030e
Yevgeniy <leonov-y@gmail.com>
2023-03-05 14:38:20 +0600

    docker setting with postgres and bash added

--- extra_commerce/extra_commerce/settings.py
+++ extra_commerce/extra_commerce/settings.py
@@ -88,10 +88,18 @@ ASGI_APPLICATION = "extra_commerce.asgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty123',
+        'HOST': 'ecommerce-db',
+        'PORT': '5432',
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
@@ -162,7 +170,7 @@ SIMPLE_JWT = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://ecommerce-redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
@@ -191,7 +199,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("ecommerce-redis", 6379)],
         },
     },
 }   
